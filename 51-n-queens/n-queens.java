c7e1d8b433e37b227b6158427329aba8f89ff4d1
Ponraj D <ponrajsdr@gmail.com>
2026-06-22 12:16:09 +0530

    Time: 2 ms (83.74%) | Memory: 46.5 MB (81.32%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -2,14 +2,17 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans=new ArrayList<>();
         char[][] board=new char[n][n];
-        for(char i[]:board){
-            Arrays.fill(i,'.');
+        for(char c[]:board){
+            Arrays.fill(c,'.');
         }
-        solve(0,board,ans);
+        boolean[] rows=new boolean[n];
+        boolean[] leftd=new boolean[2*n-1];
+        boolean[] rightd=new boolean[2*n-1];
+        solve(0,board,rows,leftd,rightd,ans,n);
         return ans;
     }
-    public void solve(int col, char[][] board, List<List<String>> ans){
-        if(col==board.length){
+    public void solve(int col, char[][] board,boolean rows[],boolean leftd[],boolean rightd[], List<List<String>> ans,int n){
+        if(col==n){
             List<String> temp=new ArrayList<>();
             for(char c[] : board){
                 StringBuilder sb=new StringBuilder();
@@ -22,37 +25,21 @@ class Solution {
             return;
         }
         for(int row=0;row<board.length;row++){
-            if(isValid(row,col,board)){
-                board[row][col]='Q';
-                solve(col+1,board,ans);
-                board[row][col]='.';
+            int d1=row+col;
+            int d2=n-1+row-col;
+            if(rows[row]||leftd[d1]||rightd[d2]){
+                continue;
             }
+            rows[row]=true;
+            board[row][col]='Q';
+            leftd[d1]=true;
+            rightd[d2]=true;
+            solve(col+1,board,rows,leftd,rightd,ans,n);
+             rows[row]=false;
+            board[row][col]='.';
+            leftd[d1]=false;
+            rightd[d2]=false;
         }
     }
-    public boolean isValid(int row,int col,char[][] board){
-        int r=row;
-        int c=col;
-        //left top
-        while(r>=0 && c>=0){
-            if(board[r][c]=='Q') return false;
-            r--;
-            c--;
-        }
-        r=row;
-        c=col;
-        //same row
-        while(c>=0){
-            if(board[r][c]=='Q') return false;
-            c--;
-        }
-        r=row;
-        c=col;
-        //left bottom
-        while(r<board.length && c>=0){
-            if(board[r][c]=='Q') return false;
-            r++;
-            c--;
-        }
-        return true;
-    }
+
 }
