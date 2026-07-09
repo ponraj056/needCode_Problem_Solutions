08b27d0a0dea3ee06a3d2121e8201541d7a99e60
Ponraj D <ponrajsdr@gmail.com>
2026-07-09 12:37:58 +0530

    Time: 331 ms (94.03%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,9 +1,6 @@
 # Write your MySQL query statement below
 select employee_id from Employees where salary<30000
  &&
-
 manager_id not in(select employee_id from Employees)
--- &&
--- manager_id is not null
 
 order by employee_id;
