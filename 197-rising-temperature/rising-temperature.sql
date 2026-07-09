a6ec4c535bae40251bdcd446ac6e7f40d1364400
Ponraj D <ponrajsdr@gmail.com>
2026-07-09 10:40:12 +0530

    Time: 481 ms (72.90%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,12 +1,5 @@
 # Write your MySQL query statement below
--- select id from Weather order by temperature desc limit 2;
- 
-select a.id
-
-from Weather a 
-
-JOIN Weather b
-
-ON DATE_ADD(b.recordDate , INTERVAL 1 DAY )= a.recordDate 
-
-WHERE a.temperature>b.temperature 
+SELECT w1.id FROM Weather w1, Weather r WHERE 
+DATEDIFF(w1.recordDate,r.recordDate)=1 
+AND
+w1.temperature > r.temperature;
