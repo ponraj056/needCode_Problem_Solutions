ace13ca63c752227611f3fa09a40d313d12dd6b0
Ponraj D <ponrajsdr@gmail.com>
2026-07-22 21:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (41.22%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public bool RotateString(string s, string goal) {
+        if(s.Length!=goal.Length){
+            return false;
+        }
+        string temp=s+s;
+      return temp.Contains(goal);
+    }
+}
